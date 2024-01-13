d1d7f747af35e8986bae3fa8c4e8b453fa1c208c
nik-rimor <nickkekes@gmail.com>
2024-01-13 07:30:48 +0200

    convert to https connections

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
-
 const express = require("express");
+const helmet = require("helmet");
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get("/secret", (req, res) => {
   res.send("Your personal secret value is 42!");
@@ -14,6 +18,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
