afd0506845529154639eb2e00afc925ae363aa01
nik-rimor <nickkekes@gmail.com>
2024-01-14 07:27:37 +0200

    prepare use of social auth and set dotenv

--- server.js
+++ server.js
@@ -4,13 +4,37 @@ const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 
+require("dotenv").config();
+
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.evv("CLIENT_ID"),
+  CLIENT_SECRET: process.env("CLIENT_SECRET"),
+};
 
 const app = express();
 
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
+// check if user is logged in
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (res, req) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   res.send("Your personal secret value is 42!");
 });
 
